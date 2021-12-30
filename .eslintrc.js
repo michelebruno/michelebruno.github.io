5560fb3b2e3eb13c22bda8f9031f1d718afe5616
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-12-30 17:51:35 +0100

    Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-    ],
-    rules: {
-        'react/jsx-filename-extension': 'off',
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/require-default-props': 'off',
-        'react/jsx-props-no-spreading': 'off',
-    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
 };
