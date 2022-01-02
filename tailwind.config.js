d4ea622e3e644955eed27c6a52e36a18d1209549
michelebruno <43304601+michelebruno@users.noreply.github.com>
2022-01-02 18:00:56 +0100

    tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  mode: 'jit',
-  // These paths are just examples, customize them to match your project structure
-  purge: [
+  content: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue,css,mdx}',
     './src/**/**/*.{js,jsx,ts,tsx,vue,css,mdx}',
   ],
-  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       gridTemplateColumns: {
         // 'project': '3fr 4.5fr 4.5fr',
         project: '0.6fr 1.2fr 1.2fr',
         home: 'repeat(5, 0.6fr)',
+        landing: '1.61fr 1fr',
       },
       colors: {
         black: '#242424',
+      },
+      borderColor: {
+        DEFAULT: 'rgba(36,36,36,0.5)',
       },
     },
     fontFamily: {
@@ -26,5 +27,4 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
 };
