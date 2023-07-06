e047562d1448430584301936df7d1b11728e968a
michelebruno <43304601+michelebruno@users.noreply.github.com>
2023-07-07 01:01:50 +0200

    studio updates

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,85 +1,73 @@
 import blockContent from './blockContent'
 
-import {defineField, defineType} from "sanity";
+import {SchemaTypeDefinition} from "sanity";
 
-export const schemaTypes = [
-  defineType({
+export const schemaTypes: SchemaTypeDefinition[] = [
+  {
     name: 'project',
     type: 'document',
     title: 'Project',
-    fields: [
-      defineField({
-        title: 'Title',
-        name: 'title',
-        type: 'string'
-      }),
-      defineField({
-        title: 'Slug',
-        name: 'slug',
-        type: 'slug',
-      }),
-      defineField({
-        title: 'type',
-        name: 'type',
-        type: 'string',
-        options: {
-          list: ['Poster', 'Book', 'Website', 'App', 'Installation', 'Video', 'Other']
-        }
-      }),
-      defineField({
-        title: 'Tagline',
-        name: 'tagline',
-        type: 'string'
-      }),
-      defineField({
-        title: 'Description',
-        name: 'description',
-        type: 'text'
-      }),
-      defineField({
-        title: 'URL',
-        name: 'websiteUrl',
-        type: 'url'
-      }),
-      defineField({
-        title: 'client',
-        name: 'client',
-        type: 'string'
-      }),
-      defineField({
-        title: 'year',
-        name: 'year',
-        type: 'string'
-      }),
-      defineField({
-        title: 'roles',
-        name: 'roles',
-        type: 'array',
-        of: [{type: 'string'}]
-      }),
-      defineField({
-        title: 'Public',
-        name: 'isPagePublic',
-        type: 'boolean'
-      }),
-      defineField({
-        title: 'Main image',
-        name: 'coverImage',
-        type: 'image',
-      }),
-      defineField({
-        title: 'thumbnail',
-        name: 'thumbnail',
-        type: 'image',
-      }),
-      defineField({
-        title: 'Images',
-        name: 'images',
-        type: 'array',
-        of: [{type: 'image'}]
-      })
+    fields: [{
+      title: 'Title',
+      name: 'title',
+      type: 'string'
+    }, {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+    }, {
+      title: 'type',
+      name: 'type',
+      type: 'string',
+      options: {
+        list: ['Poster', 'Book', 'Website', 'App', 'Installation', 'Video', 'Other']
+      }
+    }, {
+      title: 'Tagline',
+      name: 'tagline',
+      type: 'string'
+    }, {
+      title: 'Description',
+      name: 'description',
+      type: 'text'
+    }, {
+      title: 'URL',
+      name: 'websiteUrl',
+      type: 'url'
+    }, {
+      title: 'client',
+      name: 'client',
+      type: 'string'
+    }, {
+      title: 'year',
+      name: 'year',
+      type: 'string'
+    }, {
+      title: 'roles',
+      name: 'roles',
+      type: 'array',
+      of: [{type: 'string'}]
+    }, {
+      title: 'Public',
+      name: 'isPagePublic',
+      type: 'boolean'
+    }, {
+      title: 'Main image',
+      name: 'coverImage',
+      type: 'image',
+    }, {
+      title: 'thumbnail',
+      name: 'thumbnail',
+      type: 'image',
+    }, {
+      title: 'Images',
+      name: 'images',
+      type: 'array',
+      of: [{type: 'image'}]
+    }
 
     ]
-  }),
-  blockContent
+  },
+  blockContent,
+
 ]
